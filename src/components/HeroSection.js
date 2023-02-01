9fe91e007d5712c9d29865a0db8408ea54321df4
vaishalparikh <vaishalsparikh@gmail.com>
2023-02-01 13:42:31 +0530

    First Commit

---
+++ src/components/HeroSection.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <h1>Welcome To DataDao</h1>
+    </div>
+  );
+}
+
+export default HeroSection;
